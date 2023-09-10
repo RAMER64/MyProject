d175915a00ce44ae3de22ac7262aeac825230f06
RAMER64 <128093770+RAMER64@users.noreply.github.com>
2023-09-10 18:45:00 +0300

    Add files via upload

--- JS/switch.js
+++ JS/switch.js
@@ -1,22 +1,63 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let element = document.querySelector('.switch');
-  let element1 = document.querySelector('.switchBox');
-  let element2 = document.querySelector('body');
+  // Код будет выполняться, когда структура HTML-документа полностью загрузится.
+
+  // Получаем ссылки на элементы страницы
+  let switchButton = document.querySelector('.switch'); // Кнопка переключения темы
+  let switchButtonBox = document.querySelector('.switchBox'); // Обертка для кнопки
+  let body = document.querySelector('body'); // Тело документа
+  let discriptionBoxes = document.querySelectorAll('.infoBox'); // Все контейнеры с классом .infoBox
+  let photoBox = document.querySelector('.headPhoto'); // Контейнер для фотографии
+  let lightMainBox = document.querySelector('.navigationMainBox');
+  let AButtons = document.querySelectorAll('.AButtons');
+  let headNavigationBoxLight = document.querySelector('.headNavigationBox');
+  let spanLineBlack = document.querySelectorAll('.spanLine');
+  let spanBorder = document.querySelectorAll('.spanRadius');
+
+  // Получаем текущую тему из локального хранилища
   const currentTheme = localStorage.getItem("theme");
-  if (currentTheme == "light") {
-    element.classList.toggle("switchON");
-    element1.classList.toggle("switchBoxON");
-    element2.classList.toggle("bodyLight");
+  if (currentTheme === "light") {
+    toggleThemeClasses(); // Если тема светлая, сразу применяем соответствующие классы
   }
+
+  // Добавляем обработчик события на клик по переключателю темы
   document.querySelector('[href="#Switch"]').addEventListener('click', function () {
-    element.classList.toggle("switchON");
-    element1.classList.toggle("switchBoxON");
-    element2.classList.toggle("bodyLight");
-    let theme = "dark";
-    if (element2.classList.contains("bodyLight")) {
-      theme = "light";
-    }
-    localStorage.setItem("theme", theme);
-    console.log(theme)
+    toggleThemeClasses(); // Вызываем функцию для переключения классов и темы
+
+    // Определяем новую тему
+    let theme = body.classList.contains("bodyLight") ? "light" : "dark";
+    localStorage.setItem("theme", theme); // Сохраняем выбранную тему в локальном хранилище
   });
+
+  // Функция для переключения классов и темы
+  function toggleThemeClasses() {
+    // Используем метод .toggle() для добавления/удаления классов
+    switchButton.classList.toggle("switchON"); // Переключаем класс .switchON
+    switchButtonBox.classList.toggle("switchBoxON"); // Переключаем класс .switchBoxON
+    body.classList.toggle("bodyLight"); // Переключаем класс .bodyLight
+    lightMainBox.classList.toggle("navigationMainBox");
+    lightMainBox.classList.toggle("lightMainBox");
+    headNavigationBoxLight.classList.toggle("headNavigationBoxLight");
+    
+    // Переключаем классы для всех контейнеров .infoBox
+    discriptionBoxes.forEach(function(box) {
+      box.classList.toggle("discriptionElementWindow"); // Переключаем класс .discriptionElementWindow
+      box.classList.toggle("infoBox"); // Переключаем класс .infoBox
+    });
+    AButtons.forEach(function(button){
+      button.classList.toggle("mainButtonsLight");
+      button.classList.toggle("AButtons");
+    });
+    spanLineBlack.forEach(function(span){
+      // span.classList.toggle("spanRadius");
+      span.classList.toggle("spanLine");
+      span.classList.toggle("spanLineBlack");
+    });
+    spanBorder.forEach(function(spanBorder){
+      spanBorder.classList.toggle("spanBorder");
+    });
+
+    // Переключаем классы для контейнера с фотографией
+    photoBox.classList.toggle("headPhoto"); // Переключаем класс .headPhoto
+    photoBox.classList.toggle("headPhotoLight"); // Переключаем класс .headPhotoLight
+  }
 });
